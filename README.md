9372480cd0db37d250c3b884e225357c06041bdd
Joe_Yi <joe_yi@epam.com>
2025-04-01 17:35:56 +0800

    add more plugin

---
+++ README.md
@@ -0,0 +1,13 @@
+task 1 examples: 
+https://github.com/microsoft/semantic-kernel-java/blob/main/samples/semantickernel-demos/semantickernel-spring-starter/src/main/java/com/microsoft/semantickernel/starter/SemanticKernelAutoConfiguration.java
+https://github.com/microsoft/semantic-kernel-java/blob/main/samples/semantickernel-concepts/semantickernel-syntax-examples/src/main/java/com/microsoft/semantickernel/samples/syntaxexamples/chatcompletion/Example63_ChatCompletionPrompts.java
+
+
+https://learn.microsoft.com/en-us/semantic-kernel/get-started/quick-start-guide?pivots=programming-language-java
+
+
+task 2 examples:
+https://learn.microsoft.com/en-us/semantic-kernel/concepts/ai-services/chat-completion/chat-history?pivots=programming-language-java
+https://docs.spring.io/spring-ai/reference/api/chatclient.html#_chat_memory
+
+
---
+++ src/main/java/com/epam/training/gen/ai/plugins/TimePlugin.java
@@ -0,0 +1,69 @@
+package com.epam.training.gen.ai.plugins;
+
+import com.microsoft.semantickernel.semanticfunctions.annotations.DefineKernelFunction;
+import com.microsoft.semantickernel.semanticfunctions.annotations.KernelFunctionParameter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.springframework.util.StringUtils.parseLocale;
+
+@Slf4j
+public class TimePlugin {
+    public static final String DAY_MONTH_DAY_YEAR = "EEEE, MMMM d, yyyy";
+
+    /**
+     * Get the current date and time for the system default timezone.
+     *
+     * @return a ZonedDateTime object with the current date and time.
+     */
+    public ZonedDateTime now() {
+        return ZonedDateTime.now(ZoneId.systemDefault());
+    }
+
+    /**
+     * Get the current date.
+     *
+     * <p>Example: {{time.date}} => Sunday, January 12, 2025
+     *
+     * @return The current date.
+     */
+    @DefineKernelFunction(
+            name = "date",
+            description = "Get the current date")
+    public String date(
+            @KernelFunctionParameter(
+                    name = "locale",
+                    description = "Locale to use when formatting the date",
+                    required = false)
+            String locale) {
+        log.info("Getting the current date.");
+        // Example: Sunday, 12 January, 2025
+        return DateTimeFormatter.ofPattern(DAY_MONTH_DAY_YEAR)
+                .format(now());
+    }
+
+    /**
+     * Get the current time.
+     *
+     * <p>Example: {{time.time}} => 9:15:00 AM
+     *
+     * @return The current time.
+     */
+    @DefineKernelFunction(
+            name = "time",
+            description = "Get the current time")
+    public String time(
+            @KernelFunctionParameter(
+                    name = "locale",
+                    description = "Locale to use when formatting the date",
+                    required = false)
+            String locale) {
+        log.info("Getting the current time.");
+        // Example: 09:15:07 PM
+        return DateTimeFormatter.ofPattern("hh:mm:ss a")
+                .format(now());
+    }
+}
